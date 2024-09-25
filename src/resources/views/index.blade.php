25c4873f2cdef099bfeae51952f8f765d8f2b2b3
risamatsumoto1104 <risashibata114@gmail.com>
2024-09-25 22:12:22 +0900

    20240925_セットアップ途中

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="{{asset('css/sanitize.css')}}">
+    <link rel="stylesheet" href="{{asset('css/index.css')}}">
+    @yield('css')
+</head>
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <a class="header__logo" href="/">Contact Form</a>
+        </div>
+    </header>
+
+    <main>
+        <div class="contact-form__content">
+            <div>
+                <h2>お問い合わせ</h2>
+            </div>
+        </div>
+    </main>
+    
+</body>
+</html>
